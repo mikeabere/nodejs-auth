586a9c43f26df25a7a95cb43f6f604bc30abf928
mike mwanzi <mikeabere@gmail.com>
2026-03-29 21:08:58 +0300

    small change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
 
-app.use(express.json());
+app.use(express.json()); //to be able to use json
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors());
